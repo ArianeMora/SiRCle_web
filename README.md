14777f2bd3b0a7d3d2ee5496300f917b387cdf8d
Ariane Mora <ariane.mora@uq.net.au>
2022-11-17 13:50:37 +1000

    Added files to run

---
+++ README.md
@@ -0,0 +1,9 @@
+# A Beta project for SiRCle web application
+
+Streamlit app based on: https://share.streamlit.io/streamlit/example-app-csv-wrangler/
+
+### Developers
+
+```
+streamlit run app.py
+```
--- app.py
+++ app.py
@@ -5,19 +5,18 @@ import matplotlib.pyplot as plt
 from sircle import SciRCM
 from matplotlib import rcParams
 from threading import Thread
+from sciviso import Sankeyplot
 
 ###################################
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
 from streamlit.runtime.scriptrunner import add_script_run_ctx
-from st_aggrid import GridUpdateMode, DataReturnMode
-
-###################################
-
 from functionforDownloadButtons import download_button
-
 ###################################
+
+# """
+# The base of this app was developed from:
+# https://share.streamlit.io/streamlit/example-app-csv-wrangler/
+# """
+
 
 def _max_width_():
     max_width_str = f"max-width: 1800px;"
@@ -32,12 +31,12 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 
-st.set_page_config(page_icon="✂️", page_title="SiRCle")
+#st.set_page_config(page_icon="✂️", page_title="SiRCle")
 
-st.image(
-    "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/scissors_2702-fe0f.png",
-    width=100,
-)
+# st.image(
+#     "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/scissors_2702-fe0f.png",
+#     width=100,
+# )
 
 st.title("Signature Regulatory Clustering Model")
 
@@ -89,6 +88,7 @@ with c3:
 
 padd3, c0, padd4 = st.columns([1, 6, 1])
 
+
 def rcm_runner():
     st.info(f'Running SiRCle using your files! Results will appear below shortly.')
 
@@ -104,7 +104,7 @@ def rcm_runner():
                  output_dir='.',
                  non_coding_genes=['None'],
                  output_filename='SiRCLe',
-                 bg_type = '(P&M)|(P&R)|(M&R)'
+                 bg_type='(P&M)|(P&R)|(M&R)'
              )
 
     rcm.run()
@@ -116,26 +116,33 @@ def rcm_runner():
         "Download SiRCle clustering to CSV",
     )
 
-    # # figure size in inches
-    rcParams['figure.figsize'] = 3, 2
-    sns.set(rc={'figure.figsize': (3, 2)}, style='ticks')
+    plot_df = df[df['Regulation_Grouping_2'] != 'None']
+    sk = Sankeyplot(plot_df)
+    fig = sk.plot(columns=['Methylation', 'RNA', 'Protein', 'Regulation_Grouping_2'], colour_col='Protein')
+    st.plotly_chart(fig)
+    plot_bar = False
+    if plot_bar:
+        # # figure size in inches
+        rcParams['figure.figsize'] = 3, 2
+        sns.set(rc={'figure.figsize': (3, 2)}, style='ticks')
 
-    colour_map = {'MDS': '#d8419b', 'MDS_TMDE': '#e585c0', 'MDS_ncRNA': '#d880b4',
-                  'MDE': '#6aaf44', 'MDE_TMDS': '#0e8e6d', 'MDE_ncRNA': '#9edb77',
-                  'TMDE': '#fe2323', 'TMDS': '#2952ff',
-                  'TPDE': '#e68e25', 'TPDE_TMDS': '#844c0f',
-                  'TPDS': '#462d76', 'TPDS_TMDE': '#9b29b7'}
+        colour_map = {'MDS': '#d8419b', 'MDS_TMDE': '#e585c0', 'MDS_ncRNA': '#d880b4',
+                      'MDE': '#6aaf44', 'MDE_TMDS': '#0e8e6d', 'MDE_ncRNA': '#9edb77',
+                      'TMDE': '#fe2323', 'TMDS': '#2952ff',
+                      'TPDE': '#e68e25', 'TPDE_TMDS': '#844c0f',
+                      'TPDS': '#462d76', 'TPDS_TMDE': '#9b29b7'}
 
-    sns.set_palette("Greys_r")
-    rcm_labels = ["MDS", "MDS_TMDE", "MDE", "MDE_TMDS", "TMDE", "TMDS", "TPDE", "TPDE_TMDS", "TPDS", "TPDS_TMDE"]
-    colours = [colour_map[c] for c in rcm_labels]
+        sns.set_palette("Greys_r")
+        rcm_labels = ["MDS", "MDS_TMDE", "MDE", "MDE_TMDS", "TMDE", "TMDS", "TPDE", "TPDE_TMDS", "TPDS", "TPDS_TMDE"]
+        colours = [colour_map[c] for c in rcm_labels]
 
-    g = sns.catplot(data=df, x='Regulation_Grouping_2', kind="count", order=rcm_labels,
-                    palette=sns.color_palette(colours),
-                    height=4)
-    plt.xticks(rotation=45, ha='right')
-    plt.title(f'Regulation_Grouping_2')
-    st.pyplot(plt.gcf())
+        g = sns.catplot(data=df, x='Regulation_Grouping_2', kind="count", order=rcm_labels,
+                        palette=sns.color_palette(colours),
+                        height=4)
+        plt.xticks(rotation=45, ha='right')
+        plt.title(f'Regulation_Grouping_2')
+        st.pyplot(plt.gcf())
+
     st.subheader("Done regulatory clustering! Download your results using the button above.")
 
 
@@ -144,6 +151,7 @@ def run_run():
     add_script_run_ctx(thread)
     thread.start()
     thread.join()
+
 
 with c0:
     if methylation is not None and rna is not None and protein is not None:
@@ -156,71 +164,4 @@ with c0:
                Upload an DNA methylation dCpG file, a RNA file, and a protein file. 
             """
         )
-
         st.stop()
-# print(shows.columns)
-# st.subheader("How Do You Rate Your Reads?")
-# plt.hist(shows['CD8- Inflamed'].values)
-# st.pyplot(plt.gcf())
-# https://stackoverflow.com/questions/2905965/creating-threads-in-python
-
-#
-# c29, c30, c31 = st.columns([1, 1, 2])
-#
-#
-# with c29:
-#     CSVButton = download_button(
-#         df,
-#         "File.csv",
-#         "Download to CSV",
-#     )
-#
-# with c30:
-#     CSVButton = download_button(
-#         df,
-#         "File.csv",
-#         "Download to TXT",
-#     )
-#
-#
-# if df is not None:
-#     gb = GridOptionsBuilder.from_dataframe(df)
-#     # enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-#     gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
-#     gb.configure_selection(selection_mode="multiple", use_checkbox=True)
-#     gb.configure_side_bar()
-#     gridOptions_protein = gb.build()
-#
-#     response = AgGrid(
-#         df,
-#         gridOptions=gb,
-#         enable_enterprise_modules=True,
-#         update_mode=GridUpdateMode.MODEL_CHANGED,
-#         data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-#         fit_columns_on_grid_load=False,
-#     )
-#
-#     st.subheader("Regulatory clustering results below 👇 ")
-#     st.text("")
-
-
-#
-# response_m = AgGrid(
-#     methylation,
-#     gridOptions=gridOptions_meth,
-#     enable_enterprise_modules=True,
-#     update_mode=GridUpdateMode.MODEL_CHANGED,
-#     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-#     fit_columns_on_grid_load=False,
-# )
-#
-# response_r = AgGrid(
-#     rna,
-#     gridOptions=gridOptions_rna,
-#     enable_enterprise_modules=True,
-#     update_mode=GridUpdateMode.MODEL_CHANGED,
-#     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-#     fit_columns_on_grid_load=False,
-# )
-#
-
